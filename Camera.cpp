4fdf2c7f9fbe117f9eed51b2ac330c0de4a39917
Natanael-2544 <natanael.ghiura@s.unibuc.ro>
2025-12-19 16:14:04 +0200

    actualizare3

---
+++ Camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nati on 12/19/2025.
+//
+
+#include "Camera.h"
---
+++ Camera.h
@@ -0,0 +1,8 @@
+//
+// Created by Nati on 12/19/2025.
+//
+
+#ifndef OOP_CAMERA_H
+#define OOP_CAMERA_H
+
+#endif //OOP_CAMERA_H
