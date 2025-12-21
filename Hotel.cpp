a56bc4101b5977c14749f0bfa891eba59487fbc3
Natanael-2544 <natanael.ghiura@s.unibuc.ro>
2025-12-21 23:50:28 +0200

    Header Hotel

--- Hotel.cpp
+++ Hotel.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "Hotel.h"
+
+Hotel& Hotel::getInstance() {
+    static Hotel h;
+    return h;
+}
+
+void Hotel::adaugaCamera(Camera* c) {
+    camere.push_back(c);
+}
+
+void Hotel::adaugaAngajat(Angajat* a) {
+    angajati.push_back(a);
+}
+
+void Hotel::afisareAngajati() {
+    int recep = 0, menajer = 0, manager = 0;
+    for (auto a : angajati) {
+        if (dynamic_cast<Receptioner*>(a)) recep++;
+        if (dynamic_cast<Menajer*>(a)) menajer++;
+        if (dynamic_cast<Manager*>(a)) manager++;
+    }
+    std::cout << "Receptioneri:" << recep
+              << " Menajeri:" << menajer
+              << " Manageri:" << manager << "\n";
+}
+
+void Hotel::afisareRezervari() {
+    if (camere.empty()) {
+        std::cout << "Nu exista rezervari.\n";
+        return;
+    }
+    std::cout << "Rezervari hotel:\n";
+    for (auto c : camere) {
+        std::cout << *c;
+    }
+}
+
+void Hotel::afisareCamereDescrescator() {
+    if (camere.empty()) {
+        std::cout << "Nu exista rezervari.\n";
+        return;
+    }
+    std::vector<Camera*> copie = camere;
+    std::sort(copie.begin(), copie.end(), [](Camera* a, Camera* b) {
+        return a->calcPret() > b->calcPret();
+    });
+    std::cout << "Camere ordonate descrescator dupa pret:\n";
+    for (auto c : copie) {
+        std::cout << *c;
+    }
+}
+
+std::vector<Angajat*>& Hotel::getAngajati() {
+    return angajati;
+}
+
+std::vector<Camera*>& Hotel::getCamere() {
+    return camere;
+}
+
+void Hotel::afisareAngajatiDetaliat() {
+    std::cout << "angajati = [";
+    for (size_t i = 0; i < angajati.size(); i++) {
+        Angajat* a = angajati[i];
+        if (dynamic_cast<Receptioner*>(a)) std::cout << "Receptioner";
+        else if (dynamic_cast<Menajer*>(a)) std::cout << "Menajer";
+        else if (dynamic_cast<Manager*>(a)) std::cout << "Manager";
+
+        std::cout << "(" << a->getEnergie() << ")";
+        if (i != angajati.size() - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]\n";
+}
--- Hotel.h
+++ Hotel.h
@@ -5,9 +5,37 @@
 #ifndef OOP_HOTEL_H
 #define OOP_HOTEL_H
 
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "Camera.h"
+#include "Angajat.h"
+#include "Receptioner.h"
+#include "Menajer.h"
+#include "Manager.h"
 
 class Hotel {
-};
+    Hotel() = default;
 
+protected:
+    std::vector<Camera*> camere;
+    std::vector<Angajat*> angajati;
+
+public:
+    Hotel(const Hotel&) = delete;
+    Hotel& operator=(const Hotel&) = delete;
+
+    static Hotel& getInstance();
+
+    void adaugaCamera(Camera* c);
+    void adaugaAngajat(Angajat* a);
+    void afisareAngajati();
+    void afisareRezervari();
+    void afisareCamereDescrescator();
+    void afisareAngajatiDetaliat();
+
+    std::vector<Angajat*>& getAngajati();
+    std::vector<Camera*>& getCamere();
+};
 
 #endif //OOP_HOTEL_H
